e192c9d29ebf4a3ea7f882821545043ade5ae267
Tsung-Ju Chiang <71379180+tsung-ju@users.noreply.github.com>
2025-01-06 21:40:13 -0500

    Add module list

--- src/masters-thesis/agda/agda.11tydata.js
+++ src/masters-thesis/agda/agda.11tydata.js
@@ -1,5 +1,59 @@
-module.exports = {
-  layout: "agda",
-  templateEngineOverride: false,
-  permalink: (data) => `${data.page.filePathStem}.html`,
+const fs = require("fs");
+const path = require("path");
+const { finished } = require("stream/promises");
+const { WritableStream: ParserStream } = require("htmlparser2/WritableStream");
+
+async function getModules() {
+  const modules = [];
+  let currentModule = null;
+  const parserStream = new ParserStream({
+    onopentag(name, attributes) {
+      if (name === "a" && attributes.class === "Module") {
+        currentModule = {
+          href: attributes.href,
+          name: ""
+        };
+      }
+    },
+    ontext(text) {
+      if (currentModule !== null) {
+        currentModule.name += text;
+      }
+    },
+    onclosetag(name) {
+      if (name === "a" && currentModule !== null) {
+        modules.push(currentModule);
+        currentModule = null;
+      }
+    }
+  });
+  const stream = fs.createReadStream(path.join(__dirname, "Everything.html"));
+  stream.pipe(parserStream);
+  await finished(stream);
+  return modules;
+}
+
+function toTrie(modules) {
+  const trie = new Map();
+  for (const module of modules) {
+    let current = trie;
+    for (const part of module.name.split(".")) {
+      if (!current.has(part)) {
+        current.set(part, new Map());
+      }
+      current = current.get(part);
+    }
+    current.href = module.href;
+  }
+  return trie;
+}
+
+
+module.exports = async function () {
+  return {
+    layout: "agda",
+    templateEngineOverride: false,
+    permalink: (data) => `${data.page.filePathStem}.html`,
+    agdaModules: toTrie(await getModules())
+  }
 }
